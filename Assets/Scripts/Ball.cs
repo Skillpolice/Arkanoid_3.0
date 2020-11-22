7fe8e9c633193f43fe1b5475886513df4c8473c3
Denis Romanovskiy <53230856+Skillpolice@users.noreply.github.com>
2020-11-22 16:08:53 +0300

    !

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //Ссылка на обьект 
+    public Rigidbody2D rb; // доступ к обьекту из unity // и перетаскиваем обьект (скрипт мяча) на обьект в Unity , что бы получить его же Rigidbody
+    Pad pad; //ссылка(скрипт) на платформу, что бы мячь ездил вместе с платформой
+
+    [HideInInspector]
+    public bool isStarted;
+
+    public float speedBall;
+    bool IsMagnetActive;
+
+    float yPosition;
+    //float xDelta;
+
+
+    private void Start()
+    {
+        pad = FindObjectOfType<Pad>();
+
+        yPosition = transform.position.y;
+        //xDelta = transform.position.x - pad.transform.position.x; //прилипание мяча в позиции платформы
+
+        if (pad.autoPlay)
+        {
+            StartBall();
+        }
+    }
+
+
+    private void Update()
+    {
+        if (isStarted)
+        {
+
+        }
+        else
+        {
+            Vector3 padPosition = pad.transform.position; //позиция платформы 
+            Vector3 ballNewPosition = new Vector3(padPosition.x, yPosition, 0); //новая позиция меча
+            transform.position = ballNewPosition;
+
+            if (Input.GetMouseButtonDown(0)) //нажатие мыши left для полета меча
+            {
+                StartBall();
+                return;
+            }
+        }
+    }
+
+    public void StartBall() //
+    {
+        float randX = Random.Range(0, 0);
+        Vector2 force = new Vector2(randX, 5).normalized * speedBall;
+        rb.velocity = force;   //создаем дивежие меча по координатам через AddForce
+        isStarted = true;
+    }
+    public void RestartBall()
+    {
+        isStarted = false;
+        rb.velocity = Vector2.zero;
+    }
+
+   
+}
---
+++ Assets/Scripts/Pad.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pad : MonoBehaviour
+{
+    Ball ball;
+    GameManager gameManager;
+
+    public bool autoPlay;
+    public float maxX;
+
+    float yPosition;
+
+
+    void Start()
+    {
+        ball = FindObjectOfType<Ball>();
+        gameManager = FindObjectOfType<GameManager>();
+
+        yPosition = transform.position.y; // 3апоминаем позицию для платформы по Y
+        Cursor.visible = false;
+    }
+
+
+    public void Update()
+    {
+
+        //if (gameManager.isPauseActive)
+        //{
+        //    //пауза активна - ничего не нужно делать
+        //    return;
+        //}
+
+        Vector3 padNewPosition;
+        if (autoPlay)
+        {
+            Vector3 ballPos = ball.transform.position; //позиция мача 
+            padNewPosition = new Vector3(ballPos.x, yPosition, 0); //новая позиция pad
+           
+        }
+        else
+        {
+            Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // находим координаты мыши в игровом экране координат
+            padNewPosition = new Vector3(mouseWorldPosition.x, yPosition, 0); //Запоминаем двиежение платформы только  по X влево вправо и всё
+        
+        }
+        padNewPosition.x = Mathf.Clamp(padNewPosition.x, -maxX, maxX);
+        transform.position = padNewPosition; // Занесли координаты мыши в платфору двигаем её за мышкой
+    }
+}
