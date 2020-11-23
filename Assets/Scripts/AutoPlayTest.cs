9605333aa4c6388c282e19acf8795c02168e6faf
Denis Romanovskiy <53230856+Skillpolice@users.noreply.github.com>
2020-11-23 17:27:15 +0300

    Pause Panel - Save

--- Assets/Scripts/AutoPlayTest.cs
+++ Assets/Scripts/AutoPlayTest.cs
@@ -17,11 +17,38 @@ public class AutoPlayTest : MonoBehaviour
         ball = FindObjectOfType<Ball>();
         pad = FindObjectOfType<Pad>();
     }
-
     public void SpeedTestX1()
     {
-
+        if (gameManager.isPauseActive)
+        {
+            Time.timeScale = 1;
+            pad.autoPlay = true;
+            gameManager.isPauseActive = false;
+            gameManager.PauseGame();
+        }
+        gameManager.panelPause.SetActive(gameManager.isPauseActive); //проверяет в каком состоянии находится пауза TRUE / FALSE
+    }
+    public void SpeedTestX2()
+    {
+        if(gameManager.isPauseActive)
+        {
+            Time.timeScale = 2;
+            pad.autoPlay = true;
+            gameManager.isPauseActive = false;
+            gameManager.PauseGame();
+        }
+        gameManager.panelPause.SetActive(gameManager.isPauseActive); //проверяет в каком состоянии находится пауза TRUE / FALSE
     }
 
-   
+    public void SpeedTestX3()
+    {
+        if (gameManager.isPauseActive)
+        {
+            Time.timeScale = 3;
+            pad.autoPlay = true;
+            gameManager.isPauseActive = false;
+            gameManager.PauseGame();
+        }
+        gameManager.panelPause.SetActive(gameManager.isPauseActive); //проверяет в каком состоянии находится пауза TRUE / FALSE
+    }
 }
